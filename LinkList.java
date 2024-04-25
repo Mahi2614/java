b2cd3346333c8099ac13ac818b8c71bf4505a91d
Mahi2614 <mahi.m.malviya04@gmail.com>
2024-04-25 17:03:39 +0530

    LL

--- LinkList.java
+++ LinkList.java
@@ -12,11 +12,13 @@ public class LinkList {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addFirst(int data) {
 
         // step-1-create new node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -32,6 +34,7 @@ public class LinkList {
 
         // step-1-create new node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -56,17 +59,220 @@ public class LinkList {
         System.out.println("null");
     }
 
+    public void add(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        if (temp == null) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        // i=idx-1;temp->prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+    }
+
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // prev :i =size-2
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;// tail.data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    public int itrSearch(int key) {
+        Node temp = head;
+        int i = 0;
+
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+
+        }
+        // key not found
+        return -1;
+    }
+
+    public int helper(Node head, int key) {
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == key) {
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if (idx == -1) {
+            return -1;
+        }
+        return idx + 1;
+    }
+
+    public int recSearch(int key) {
+        return helper(head, key);
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node curr = tail = head;// rgh to lft
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+
+    }
+
+    public void delNthfromEnd(int n) {
+        // calculate size
+        int sz = 0;
+        Node temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            sz++;
+        }
+        if (n == sz) {
+            head = head.next;
+        }
+        // sz-n
+        int i = 1;
+        int iToFind = sz - n;
+        Node prev = head;
+        while (i < iToFind) {
+            prev = prev.next;
+            i++;
+
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+
+    // Slow Fast Approach
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;// +1
+            fast = fast.next;// +2
+
+        }
+        return slow;// slow is midNode
+    }
+
+    public boolean checkPalindrome() {
+        if (head == null || head.next != null) {
+            return true;
+        }
+        // step-1
+        Node midNode = findMid(head);
+        // step2
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;// right half head
+        Node left = head;
+
+        // step3-check left hLF & right half
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
     public static void main(String args[]) {
         LinkList l1 = new LinkList();
-        l1.print();
-        l1.addFirst(1);
-        l1.print();
+        // l1.print();
         l1.addFirst(2);
+        // l1.print();
+        l1.addFirst(1);
+        // l1.print();
+        // l1.addLast(3);
+        // l1.print();
+        l1.addLast(4);
+        // l1.print();
+        l1.addLast(5);
+
+        l1.add(2, 3);
         l1.print();
-        l1.addLast(3);
+        System.out.println(l1.size);
+
+        l1.removeFirst();
         l1.print();
-        l1.addFirst(4);
+        l1.removeLast();
         l1.print();
+        System.out.println(l1.size);
+
+        System.out.println(l1.itrSearch(3));
+        System.out.println(l1.itrSearch(10));
+
+        System.out.println(l1.recSearch(3));
+        System.out.println(l1.recSearch(10));
+
+        l1.reverse();
+        l1.print();
+
+        l1.delNthfromEnd(1);
+        l1.print();
+
+        System.out.println(l1.checkPalindrome());
 
     }
 }
